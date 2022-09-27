b9bdae9939170a90caee3be8819883b3796c9ef4
Nikita Istomin <nikita.istomin54@gmail.com>
2022-09-26 23:09:16 -0500

    Initial commit/v.1

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameInput : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            ReloadLevel();
+        }
+    }
+
+    private void ReloadLevel()
+    {
+        int activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(activeSceneIndex);
+    }
+}
